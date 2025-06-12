a86326196b1f2bb2f6529de04c8295e1f7c4fd52
Garrett Lowrance <garrettlowrance12@gmail.com>
2025-06-12 15:06:03 -0400

    level complete hook
    oops

--- src/PlayLayer.cpp
+++ src/PlayLayer.cpp
@@ -8,9 +8,20 @@ class $modify(PlayLayer) {
 
 	// AFTER INCREMENT CHALLENGE
 	void showNewBest(bool newReward, int orbs, int diamonds, bool demonKey, bool noRetry, bool noTitle) {
-		int newDiamonds = diamonds + GSM::getQuestRewards();
+		int newDiamonds = diamonds + GameStatsManagerAQ::getQuestRewards();
 		PlayLayer::showNewBest(newReward, orbs, newDiamonds, demonKey, noRetry, noTitle);
 	} // showNewBest
+
+	void levelComplete() {
+		PlayLayer::levelComplete();
+
+		auto screenSize = CCDirector::sharedDirector()->getWinSize();
+		auto rewardLayer = CurrencyRewardLayer::create(
+			0, 0, 0,
+			GameStatsManagerAQ::getQuestRewards(), 
+			(CurrencySpriteType)0, 0,
+			(CurrencySpriteType)0, 0,
+			screenSize,
 			(CurrencyRewardType)0, 0, 1
 		);
 		addChild(rewardLayer);
